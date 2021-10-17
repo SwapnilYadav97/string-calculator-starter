d0b8340ca175b296fedd5cc9e7d178d87bb04b56
SwapnilYadav97 <swapnilsureshyadav97@gmail.com>
2021-10-17 22:55:50 +0530

    Test Completed Succesfully

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -7,14 +7,15 @@ import static org.junit.jupiter.api.Assertions.*;
 class StringCalculatorShould {
 
     @Test
-    void empty_string_should_return_0() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(0, stringCalculator.add(""));
+    public void
+    return_0_when_input_is_empty() {
+        assertEquals("0", given(""));
     }
 
     @Test
-    void string_with_single_number_should_return_number_as_int() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(1, stringCalculator.add("1"));
+    public void
+    return_3_when_input_is_1_2() {
+        assertEquals("3", given("1,2"));
     }
+
 }
